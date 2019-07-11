4d8fed23091d906cf5b44cb18f5dacd13cd8ea3b
liyanxun <liyanxun1128@gmail.com>
2019-07-10 23:30:10 -0700

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,12 +4,13 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 function RenderDish({ dish }) {
     return (
         <div className="col-12 col-md-5 m-1">
             <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
                     <CardText>{dish.description}</CardText>
@@ -104,7 +105,7 @@ function RenderComments({ comments, addComment, dishId }) {
             <div className="col-12 col-md-5 m-1">
                 <div>
                     <h4>Comments</h4>
-                    <ul class="list-unstyled">
+                    <ul className="list-unstyled">
                         {comments.map((comment) => {
                             return (
                                 <li key={comment.id}>
